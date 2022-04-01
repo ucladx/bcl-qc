be018ce5818e5d67f7e55caabf500e2a636eee3a
Xiavan Roopnarinesingh <xroopnar@gmail.com>
2022-04-01 12:48:08 -0700

    hamming distance and mismatch calculation utils

--- bcl-qc/samplesheet.py
+++ bcl-qc/samplesheet.py
@@ -1,7 +1,39 @@
 #!/usr/bin/env python3
+import pandas as pd 
+from Bio.Seq import Seq
+from skbio.sequence import DNA
 
 def check_barcodes():
     """
     Check for possible collisions between given barcodes
     """
+    pass
+
+
+# utilities for identifying barcode similarity/collision
+def reverse_complement(index):
+    rc = Seq(index).reverse_complement()
+    return(rc)
+
+def to_rc(index_series):
+    rc = index_series.apply(lambda x : Seq(x).reverse_complement(),axis=1)
+    return(rc)
+
+def get_hamming(a,b):
+    a = DNA(a)
+    hamming_distance = a.distance(b)
+    return(hamming_distance)
+
+def get_mismatches(a,b):
+    hamming_distance = get_hamming(a,b)
+    mismatches = round(len(a)*hamming_distance)
+    return(mismatches)
+
+def mismatch_check(a,b,n=3):
+    mismatches = get_mismatches(a,b)
+    passed = n > mismatches
+    return(passed)
+
+def pairwise_mismatch_check(df):
+    pass
     
