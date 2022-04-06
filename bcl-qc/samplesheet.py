59c8506a759a28d8e736c3582e9e2b5f4ce1a7ed
Xiavan Roopnarinesingh <xroopnar@gmail.com>
2022-04-06 10:57:02 -0700

    added arbitrary length pairwise comparison

--- bcl-qc/samplesheet.py
+++ bcl-qc/samplesheet.py
@@ -43,7 +43,8 @@ def mismatch_vector(a,series_b):
 
 def pairwise_mismatch_check(series_a,series_b):
     mismatches = series_a.apply(lambda x : mismatch_vector(x,series_b))
-    mismatches.index = mismatches.columns
+    mismatches.index = series_a.index
+    #mismatches.index = mismatches.columns
     return(mismatches)
 
 #debug utils
