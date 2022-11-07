65bbb25e79796c79ec800ac92d36cb6892f2aa2c
Ian Atol <iatol@mednet.ucla.edu>
2022-11-07 14:00:56 -0800

    Fixup new_run_watcher

--- bcl-qc/new_run_watcher.sh
+++ bcl-qc/new_run_watcher.sh
@@ -3,14 +3,14 @@ RUNS_DIR='/mnt/pns/runs/'
 while true; do
 # Watch `RUNS_DIR` for creation of new files
 inotifywait -q -m -e create $RUNS_DIR | while read DIRECTORY EVENT FILE; do
-    if [ $EVENT == 'CREATE*']
+    if [ $EVENT == 'CREATE*' ]
     then
         # Check that `FILE` is a run directory
         [[ $FILE =~ "\d{6}_[a-zA-Z0-9]{6}_\d{4}_[a-zA-Z0-9]{10}" ]] || continue
         # Now watch `RUN_DIR` for creation of "CopyComplete.txt"
         RUN_DIR=$FILE
         inotifywait -q -m -e create $RUN_DIR | while read DIRECTORY EVENT FILE; do
-            if [ $EVENT == 'CREATE*'] && [ $FILE == 'CopyComplete.txt']
+            if [ $EVENT == 'CREATE*' ] && [ $FILE == 'CopyComplete.txt' ]
             then
                 # Actually run the QC passes on RUN_DIR
                 python3 bcl_qc.py $RUN_DIR
