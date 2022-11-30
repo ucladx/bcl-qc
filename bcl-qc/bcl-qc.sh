c37869fb401fdaea68448a9cab7d41293fec421c
Ian Atol <ian.r.atol@gmail.com>
2022-11-29 16:01:03 -0800

    Re-enable all commands in bcl-qc.sh

--- bcl-qc/bcl-qc.sh
+++ bcl-qc/bcl-qc.sh
@@ -2,24 +2,24 @@ run_dir=$1
 
 ### demux
 echo "copying $run_dir to staging"
-# rsync -rhlW --info=progress2 --exclude='Thumbnail_Images' /mnt/pns/runs/$run_dir /staging/hot/$run_dir
+rsync -rhlW --info=progress2 --exclude='Thumbnail_Images' /mnt/pns/runs/$run_dir /staging/hot/$run_dir
 
 echo "setting up reads folder"
-# mkdir /staging/hot/reads/$run_dir
-# mkdir /staging/hot/reads/$run_dir/I10
+mkdir /staging/hot/reads/$run_dir
+mkdir /staging/hot/reads/$run_dir/I10
 
 echo "running demux"
-# dragen --bcl-conversion-only true --bcl-use-hw false --bcl-only-matched-reads true --bcl-input-directory /staging/hot/$run_dir --sample-sheet /staging/hot/$run_dir/SampleSheet_I10.csv --output-directory /staging/hot/reads/$run_dir/I10
+dragen --bcl-conversion-only true --bcl-use-hw false --bcl-only-matched-reads true --bcl-input-directory /staging/hot/$run_dir --sample-sheet /staging/hot/$run_dir/SampleSheet_I10.csv --output-directory /staging/hot/reads/$run_dir/I10
 
 ### align
 EXONS=/mnt/pns/tracks/ucla_mdl_cancer_ngs_v1_exon_targets.hg38.bed
 
 echo "creating bam dirs"
-# cut -f2 -d, /staging/hot/reads/$run_dir/I10/Reports/fastq_list.csv | grep -v ^RGSM | sort -u | xargs -L1 -I{} mkdir -p /mnt/pns/bams/$run_dir/{}
+cut -f2 -d, /staging/hot/reads/$run_dir/I10/Reports/fastq_list.csv | grep -v ^RGSM | sort -u | xargs -L1 -I{} mkdir -p /mnt/pns/bams/$run_dir/{}
 
 echo "running alignment"
-# cut -f2 -d, /staging/hot/reads/$run_dir/I10/Reports/fastq_list.csv | grep -v ^RGSM | sort -u | xargs -L1 -I{} dragen --intermediate-results-dir /staging/tmp --enable-map-align true --enable-map-align-output true --output-format BAM --enable-duplicate-marking true --generate-sa-tags true --enable-sort true --ref-dir /staging/human/reference/hg38_alt_masked_graph_v2 --qc-coverage-tag-1 target_bed --qc-coverage-region-1 $EXONS --qc-coverage-reports-1 cov_report --qc-coverage-ignore-overlaps true --enable-variant-caller true --vc-combine-phased-variants-distance 6 --vc-emit-ref-confidence GVCF --enable-hla true --fastq-list /staging/hot/reads/$run_dir/I10/Reports/fastq_list.csv --fastq-list-sample-id {} --output-directory /mnt/pns/bams/$run_dir/{} --output-file-prefix {}
+cut -f2 -d, /staging/hot/reads/$run_dir/I10/Reports/fastq_list.csv | grep -v ^RGSM | sort -u | xargs -L1 -I{} dragen --intermediate-results-dir /staging/tmp --enable-map-align true --enable-map-align-output true --output-format BAM --enable-duplicate-marking true --generate-sa-tags true --enable-sort true --ref-dir /staging/human/reference/hg38_alt_masked_graph_v2 --qc-coverage-tag-1 target_bed --qc-coverage-region-1 $EXONS --qc-coverage-reports-1 cov_report --qc-coverage-ignore-overlaps true --enable-variant-caller true --vc-combine-phased-variants-distance 6 --vc-emit-ref-confidence GVCF --enable-hla true --fastq-list /staging/hot/reads/$run_dir/I10/Reports/fastq_list.csv --fastq-list-sample-id {} --output-directory /mnt/pns/bams/$run_dir/{} --output-file-prefix {}
 
 ### multiqc
 rm -f /mnt/pns/bams/$run_dir/*/*.wgs_*.csv
-multiqc --force --outdir /mnt/pns/bams/$run_dir /mnt/pns/bams/$run_dir /staging/hot/reads/$run_dir/I10/
+multiqc --force --config ../config/multiqc_config.yaml --outdir /mnt/pns/bams/$run_dir /mnt/pns/bams/$run_dir /staging/hot/reads/$run_dir/I10/
