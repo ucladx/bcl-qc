06b577177ffd5a27a9e84e9f5839957027121c0f
Ian Atol <iatol@mednet.ucla.edu>
2023-03-09 18:36:28 -0800

    refactor run watcher

--- bcl-qc/scripts/runs_watcher.sh
+++ bcl-qc/scripts/runs_watcher.sh
@@ -6,14 +6,7 @@ inotifywait -q -m -e create $RUNS_DIR | while read DIRECTORY EVENT FILE; do
     if [[ $EVENT == 'CREATE*' ]]; then
         # Check if the created file matches the run directory name format
         [[ $FILE =~ "\d{6}_[a-zA-Z0-9]{6}_\d{4}_[a-zA-Z0-9]{10}" ]] || continue
-        # If it does, wait until the run is finished and a SampleSheet has been generated
-        RUN_DIR=$FILE
-        inotifywait -q -m -e create $RUN_DIR | while read DIRECTORY EVENT FILE; do
-            if [[ $EVENT == 'CREATE*' ]] && [[ $FILE == 'CopyComplete.txt' ]]; then
-                python3 bcl-qc.py $RUN_DIR
-            fi
-        done
+        bash run_watcher.sh $FILE
     fi
 done
-
 done # while true
