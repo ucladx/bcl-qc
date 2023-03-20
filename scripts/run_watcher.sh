532dad42fd1682d6d362281c7284e03206cd6f54
Ian Atol <ian.r.atol@gmail.com>
2023-03-20 16:35:52 -0700

    Remove unnecessary cd cmds

--- scripts/run_watcher.sh
+++ scripts/run_watcher.sh
@@ -1,9 +1,9 @@
 RUN_DIR=$1
-
 inotifywait -q -e create $RUN_DIR | while read DIRECTORY EVENT FILE; do
+    echo $EVENT
+    echo $FILE
     if [[ $EVENT == 'CREATE' ]] && [[ $FILE == 'CopyComplete.txt' ]]; then
         parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-        cd "$parent_path"
-        python3 ../bcl-qc.py "$RUN_DIR"
+        python3 "$parent_path/../bcl-qc.py" $RUN_DIR
     fi
 done
