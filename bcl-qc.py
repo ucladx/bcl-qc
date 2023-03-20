180bbc4708dfe7e51157c7f29676fb1eaf53683f
Ian Atol <ian.r.atol@gmail.com>
2023-03-20 14:58:32 -0700

    More run watching debugging

--- bcl-qc.py
+++ bcl-qc.py
@@ -22,24 +22,21 @@ def generate_samplesheet(run_path, barcode_format):
     raise Exception(f"SampleSheet_{barcode_format}.csv not found in {run_path}")
 
 def qc_run(run_path: str, args: str):
-    print(f"bcl-qc on {run_path}")
     # if samplesheet_missing(run_path, barcode_format): generate_samplesheet(run_path, barcode_format)
     execute_passes(run_path, args)
 
 if __name__ == "__main__":
     # absolute path to a specific run's output directory
-    # run_path = sys.argv[-1]
-    # if run_path[-1] != '/':  run_path += '/'
-    # args = sys.argv[1:-1]
-    # qc_run(run_path, args)
+    run_path = sys.argv[-1]
+    if run_path[-1] != '/':  run_path += '/'
+    args = sys.argv[1:-1]
+    qc_run(run_path, args)
 
 ### TODO ###
 # Fix run watcher
 # Better file organization
 # Sample specific bed files (parse samplesheet)
 # Support passes config file? 
-
-    
 
 # Unit tests:
 #     sample sheet generation
--- scripts/run_watcher.sh
+++ scripts/run_watcher.sh
@@ -1,8 +1,9 @@
 RUN_DIR=$1
-inotifywait -q -m -e create $RUN_DIR | while read DIRECTORY EVENT FILE; do
+
+inotifywait -q -e create $RUN_DIR | while read DIRECTORY EVENT FILE; do
     if [[ $EVENT == 'CREATE' ]] && [[ $FILE == 'CopyComplete.txt' ]]; then
         parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
         cd "$parent_path"
-        python3 ../bcl-qc.py
+        python3 ../bcl-qc.py "$RUN_DIR"
     fi
 done
