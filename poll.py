b68dd257a3e23d37c69a7610e1525a7e8335771e
Ian Atol <iatol@mednet.ucla.edu>
2023-06-08 17:08:11 -0700

    Fix poll.py

--- poll.py
+++ poll.py
@@ -6,10 +6,12 @@ from bclqc import bclqc_run
 def poll_directory(path):
     while True:
         if 'CopyComplete.txt' in os.listdir(path):
-            bclqc_run(path, {})
+            bclqc_run(path.split('/')[-1], {})
             break
         else:
             time.sleep(600) # Sleep for 10 minutes
 
 directory_path = sys.argv[-1]
+if directory_path.endswith('/'):
+    directory_path = directory_path[:-1]
 poll_directory(directory_path)
